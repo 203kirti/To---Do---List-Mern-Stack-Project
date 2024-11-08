1b8879e0ccc5b45e4ac28e8ebfaff82319d3dcf1
Kirti <kirti.air203@gmail.com>
2024-11-08 23:43:27 +0530

    Created to-do-app basic functionality

---
+++ ToDoList/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Home from './Home'
+import './App.css'
+
+function App() {
+
+  
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
+
+
+/* 
+      https://www.youtube.com/watch?v=BqRWK57dwqo&t=905s
+
+*/
